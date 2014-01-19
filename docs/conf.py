3697ed2ac489275a59878d1c2f289d02b1be582d
Łukasz Kostka <ukasz83@gmail.com>
2014-01-19 20:54:09 +0100

    added unicode u''

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'librouteros'
-copyright = '2014, Łukasz Kostka'
+copyright = u'2014, Łukasz Kostka'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
